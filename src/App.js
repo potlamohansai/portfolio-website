c29078969ca528e9da997e1aa219097246bf1865
Mohan Sai Potla <potlamohansai@gmail.com>
2022-10-31 20:07:54 +0530

    hero section responsiveness completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/Home/Home';
 
@@ -9,7 +10,7 @@ function App() {
     <Header/>
     <main className='main'>
       <Home/>
-
+      <About/>
     </main>
     </>
   );
