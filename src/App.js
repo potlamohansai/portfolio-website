27812dc924abc88a503d7e0fe0cf7179fc912314
Mohan Sai Potla <potlamohansai@gmail.com>
2022-11-03 22:23:33 +0530

    skills section completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Services from './components/services/Services';
+import Skills from './components/skills/Skills';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
     <main className='main'>
       <Home/>
       <About/>
+      <Skills/>
+      <Services/>
     </main>
     </>
   );
