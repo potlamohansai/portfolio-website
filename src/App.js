adeb6a06611583fb601ce363989b991912a749f9
Mohan Sai Potla <potlamohansai@gmail.com>
2022-11-04 20:21:44 +0530

    scrollup function done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
+import Scrollup from './components/scrollup/Scrollup';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 import Testimonials from './components/testimonials/Testimonials';
@@ -22,8 +23,11 @@ function App() {
       <Qualification/>
       <Testimonials/>
       <Contact/>
-      <Footer/>
+      
     </main>
+
+    <Footer/>
+    <Scrollup/>
     </>
   );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
         <div className="footer__container container">
-            <h1 className="footer__title">Mohan Sai</h1>
+            <h1 className="footer__title">Mohan Sai Potla</h1>
 
             <ul className="footer__list">
                 <li>
