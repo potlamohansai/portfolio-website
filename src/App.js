f5c7c261885ccdc59adb8ed3065601f5f7f0bb8f
Mohan Sai Potla <potlamohansai@gmail.com>
2022-11-04 15:20:41 +0530

    testimonials section completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/home/Home';
 import Qualification from './components/qualification/Qualification';
 import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
+import Testimonials from './components/testimonials/Testimonials';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Skills/>
       <Services/>
       <Qualification/>
+      <Testimonials/>
     </main>
     </>
   );
